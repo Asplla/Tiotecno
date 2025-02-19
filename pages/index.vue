c3741a134b34bcd52c8bb417e18b1cc4489b0e58
iván <wangxu_cn@icloud.com>
2025-02-19 18:27:35 +0800

    Beta 0.0.0

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/pages/index.css'
+
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 20px;
+}
+</style> 
