8c6f7bc5dfd29aefacbedc2e031599ae8bb43406
iván <wangxu_cn@icloud.com>
2025-02-20 15:27:49 +0800

    Beta 0.0.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,5 +29,27 @@ export default defineNuxtConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+  tailwindcss: {
+    config: {
+      safelist: [
+        'space-x-1',
+        'space-x-2',
+        'space-x-3',
+        'space-x-4',
+        'space-x-5',
+        'space-x-6',
+        'space-x-7',
+        'space-x-8',
+        'space-y-1',
+        'space-y-2',
+        'space-y-3',
+        'space-y-4',
+        'space-y-5',
+        'space-y-6',
+        'space-y-7',
+        'space-y-8',
+      ]
+    }
   }
 })
